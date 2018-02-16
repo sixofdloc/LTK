24fe1b8b0354dfa2046d4883a1a37221fab8bc85
Six <sixofdloc@gmail.com>
2018-02-16 13:47:20 -0500

    adding errorhan.asm

--- SYSGEN/s1/test.bat
+++ SYSGEN/s1/test.bat
@@ -6,6 +6,7 @@ call test_file checksum checksum.r
 call test_file convrtio convrtio.r
 call test_file d d.r
 call test_file dir dir.r
+call test_file errorhan errorhan.r
 call test_file fastcpqd fastcpqd.r
 call test_file fileprot fileprot.r
 call test_file initc064 initc064.r
