86c1f7e8907e2ce6cc23e668c133c59c2705975d
David Wood <jbevren@gmail.com>
2018-02-21 09:21:44 -0500

    Label and comment, should be done now.

--- SYSGEN/s1/b.asm
+++ SYSGEN/s1/b.asm
@@ -1,26 +1,36 @@
 	.include "../../include/kernal.asm"
-	*=$0100
-	.repeat $100,$02
+	*=$0100		; autoload by crashing the stack
+	.repeat $100,$02; we start at $0203 so store $0202 for rts.
 	.byte $00,$00,$00
 START
 	sei 
-	lda #$3c	;	0204 a9 3c    
-	sta $df03	;	0206 8d 03 df 
-	sta $de03	;	0209 8d 03 de 
-	lda #$40	;	020c a9 40    
-	sta $df02	;	020e 8d 02 df 
-	sta $de02	;	0211 8d 02 de 
-	lda #$03	;	0214 a9 03    
-	ldx #<filename	;	0216 a2 2e    
-	ldy #>filename	;	0218 a0 02    
-	jsr SETNAM	;	021a 20 bd ff 
-	lda #$08	;	021d a9 08    
-	ldy #$08	;	021f a0 08    
-	ldx #$08	;	0221 a2 08    
-	jsr SETLFS	;	0223 20 ba ff 
-	lda #$00	;	0226 a9 00    
-	jsr LOAD	;	0228 20 d5 ff 
-	jmp $0810	;	022b 4c 10 08 
+	lda #$3c	; FIXME: appears to be an idle/offline state
+                        ;0011 1100
+                        ;00     ; irq off
+                        ;111    ; cb2 high
+                        ;1      ; port register on
+                        ;00     ; cb1 high
+	sta $df03	; set port b on both
+	sta $de03	;  adapters to idle/offline
+	lda #$40	;
+                        ;0010 0000
+                        ;00     ; irq off
+                        ;100    ; handshake [ca2: pulse low on read; cb2: go low on write]
+                        ;0      ; ddr on
+                        ;00     ; ca1 high
+	sta $df02	; set port a on both
+	sta $de02	;  adapters to idle state
+	lda #$03	; filename is 3 chars long
+	ldx #<filename	;
+	ldy #>filename	;
+	jsr SETNAM	; set file name
+	lda #$08	;
+	ldy #$08	;
+	ldx #$08	;
+	jsr SETLFS	; set file up (dev,sa,file #8)
+	lda #$00	;
+	jsr LOAD	; LOAD "sb2",8,8
+	jmp $0810	;  start sb2 up.
 filename
 	.text "sb2"
 	
