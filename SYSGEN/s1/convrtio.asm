ce550a462e017d31c80cdfa95ac12cc15fd71c85
David Wood <jbevren@gmail.com>
2018-04-04 15:37:39 -0400

    Initial documentation, relocation stub in

--- SYSGEN/s1/convrtio.asm
+++ SYSGEN/s1/convrtio.asm
@@ -1,12 +1,34 @@
+;****************************************
+;*                                  _    _                                
+;*    ___  ___   _ __ __   __ _ __ | |_ (_)  ___     __ _  ___  _ __ ___  
+;*   / __|/ _ \ | '_ \\ \ / /| '__|| __|| | / _ \   / _` |/ __|| '_ ` _ \ 
+;*  | (__| (_) || | | |\ V / | |   | |_ | || (_) |_| (_| |\__ \| | | | | |
+;*   \___|\___/ |_| |_| \_/  |_|    \__||_| \___/(_)\__,_||___/|_| |_| |_|
+;*                                                                        
+
 ;convrtio.r.prg
+
+; ****************************
+; * 
+; * VIM settings for David.
+; * 
+; * vim:syntax=a65:hlsearch:background=dark:ai:
+; * 
+; ****************************
+
+
 	.include "../../include/ltk_dos_addresses.asm"
 	.include "../../include/ltk_equates.asm"
+	.include "../../include/relocate.asm"	; relocation tools.
+
 	*=LTK_MiniSubExeArea ;$95e0, $4000 for sysgen
 L93e0               
-	jmp L9492
+	jmp convrtio
                     
-	.byte $af 
-L93e4
+	.byte $af ; size of patch below
+	; relocate to fa2c
+	#relocate $fa2c
+Lfa2c
 	pha
 	txa
 	pha
@@ -15,10 +37,10 @@ L93e4
 	tsx
 	lda $0104,x
 	and #$10
-	beq L93f4
+	beq Lfa3c
 	jmp ($0316)
                     
-L93f4
+Lfa3c
 	lda #$fa
 	pha
 	lda #$4b
@@ -30,57 +52,57 @@ L93f4
 	pha
 	jmp $ea31
                     
-L9403
+Lfa4b
 	sei
 	ldy #$00
 	lda $cb
 	cmp #$40
-	bne L946e
+	bne Lfab6
 	lda #$ff
 	sta $dc00
 	sty $d02f
 	lda $dc01
 	cmp #$ff
-	beq L946e
+	beq Lfab6
 	stx $c5
 	lda #$fe
-L941f
+Lfa67
 	pha
 	ldx #$08
 	sta $d02f
-L9425
+Lfa6d
 	lda $dc01
 	cmp $dc01
-	bne L9425
-L942d
+	bne Lfa6d
+Lfa75
 	lsr a
-	bcs L9439
+	bcs Lfa81
 	pha
 	lda $fac1,y
-	beq L9438
+	beq Lfa80
 	sta $cb
-L9438
+Lfa80
 	pla
-L9439
+Lfa81
 	iny
 	dex
-	bne L942d
+	bne Lfa75
 	pla
 	sec
 	rol a
 	cpy #$17
-	bcc L941f
+	bcc Lfa67
 	lda $cb
 	cmp #$40
-	beq L946e
+	beq Lfab6
 	ldx #$81
 	ldy #$00
-	bcc L9458
+	bcc Lfaa0
 	and #$7f
 	sta $cb
 	ldx #$c2
 	ldy #$01
-L9458
+Lfaa0
 	lda #$eb
    	sty $028d
    	stx $f5
@@ -88,69 +110,70 @@ L9458
    	jsr $eae0
    	ldy #$00
    	ldx #$10
-L9468 
+Lfab0 
 	iny
-	bne L9468
+	bne Lfab0
 	dex
-	bne L9468
-L946e
+	bne Lfab0
+Lfab6
 	lda #$ff
 	sta $d02f
 	jsr $eb42
 	jmp $ea81
                     
+	#endr
 L9479                                          
 	.byte $00,$1b,$10,$00,$3b,$0b,$18,$38
 	.byte $00,$28,$2b,$00,$01,$13,$20,$08
 	.byte $00,$23,$2c,$87,$07,$82,$02,$00 
 	.byte $00
 
-L9492
+convrtio
 	ldy #$00
-	jsr S94d7
+	jsr GetY_Inc		; get opcode
 	ldx #$06
 L9499
-	cmp L94db,x
-	beq L94a3
+	cmp Opcode_Table,x	; compare with table
+	beq L94a3		;  got a match?  Do some editing
 	dex
-	bpl L9499
-	bmi L94c4
+	bpl L9499		;  continue comparing
+	bmi L94c4		; done comparing, update pointers
 L94a3
-	jsr S94d7
+	jsr GetY_Inc		; get low byte
 	cmp #$04
-	bcs L94c4
-	jsr S94d7
-	cmp #$df
-	bne L94c4
-	lda #$de
-	dey
-	sta ($31),y
-	lda $31
+	bcs L94c4		; higher than 4: skip ahead
+	jsr GetY_Inc		; get high byte
+	cmp #$df		; DFxx?
+	bne L94c4		;  no, skip
+	lda #$de		; get DExx page address
+	dey			; back up one
+	sta ($31),y		; store
+	lda $31			; get pointer
 	clc
-	adc #$03
+	adc #$03		; add 3
 	sta $31
 	bcc L94c1
 	inc $32
 L94c1
 	clc
-	bcc L9492
+	bcc convrtio		; loop.
 L94c4
 	inc $31
 	bne L94ca
-	inc $32
+	inc $32			; increment pointer
 L94ca
 	lda $32
 	cmp $34
-	bne L9492
+	bne convrtio
 	lda $31
 	cmp $33
-	bcc L9492
+	bcc convrtio		; loop until we're done
 	rts
                     
-S94d7               
+GetY_Inc               
 	lda ($31),y
 	iny
 	rts
                     
-L94db
+Opcode_Table
 	.byte $8c,$8d,$8e,$ac,$ad,$ae,$2c 
