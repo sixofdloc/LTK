91c3002d57c0a5211203bfc6c0b3dae19f98f7fb
David Wood <jbevren@gmail.com>
2018-10-02 23:18:58 -0400

    Checksum.asm:  Initial documentation of code started

--- SYSGEN/s1/checksum.asm
+++ SYSGEN/s1/checksum.asm
@@ -1,3 +1,27 @@
+; ****************************
+; * 
+; * Lieutenant Kernal resource project
+; * 
+; *         _                  _                                                    
+; *    ___ | |__    ___   ___ | | __ ___  _   _  _ __ ___      __ _  ___  _ __ ___  
+; *   / __|| '_ \  / _ \ / __|| |/ // __|| | | || '_ ` _ \    / _` |/ __|| '_ ` _ \ 
+; *  | (__ | | | ||  __/| (__ |   < \__ \| |_| || | | | | | _| (_| |\__ \| | | | | |
+; *   \___||_| |_| \___| \___||_|\_\|___/ \__,_||_| |_| |_|(_)\__,_||___/|_| |_| |_|
+; *                                                                                 
+; * 
+; * Checksum verifies the integrity of the DOS
+; *  files by running a checksum on them all.
+; *
+; ****************************
+
+; ****************************
+; * 
+; * VIM settings for David.
+; * 
+; * vim:syntax=a65:hlsearch:background=dark:ai:
+; * 
+; ****************************
+
 ;checksum.r.prg
 	.include "../../include/ltk_dos_addresses.asm"
 	.include "../../include/ltk_equates.asm"
@@ -6,77 +30,84 @@
 START
 	ldx #<str_Checksumming
 	ldy #>str_Checksumming
-	jsr LTK_Print
+	jsr LTK_Print		; print Checksumming note
 	
-	lda #$0a
-	ldx #$00
-	ldy #$00
-	clc
-	jsr LTK_HDDiscDriver
-	.byte <LTK_FileHeaderBlock,>LTK_FileHeaderBlock,$01
+	lda #$0a		; LU 10
+	ldx #$00		
+	ldy #$00		; block 0
+	clc			; read
+	jsr LTK_HDDiscDriver	;  READ block to the file header area
+	.word LTK_FileHeaderBlock 
+	.byte $01		; one block
 L95f4
-	inx
+	inx			; next block
 L95f5
-	lda #$0a
-	stx $96a8
-	sty L96a7
-	clc
-	jsr LTK_HDDiscDriver
-	.byte <LTK_MiscWorkspace,>LTK_MiscWorkspace,$01
+	lda #$0a		; LU 10
+	stx lba_hi
+	sty lba_lo		; Save current block
+	clc			; read
+	jsr LTK_HDDiscDriver	;  READ block to LTK Workspace area
+	.word LTK_MiscWorkspace
+	.byte $01		; one block
 L9604
-	lda #$e0
+	lda #<LTK_MiscWorkspace
 	sta $31
-	lda #$8f
+	lda #>LTK_MiscWorkspace
 	sta $32
 	ldy #$00
 	ldx #$02
 L9610
-	lda ($31),y
-	pha
+	lda ($31),y		; get byte
+	pha			; save a copy for later
+
+	; calculate running 16-bit checksum
+	;  cs_16 = (cs_16 + byte) AND $ffff
 	clc
-	adc $96a9
-	sta $96a9
+	adc cs_16_l		; calculate low byte
+	sta cs_16_l
 	lda #$00
-	adc $96aa
-	sta $96aa
-	pla
-	clc
-	adc $96ad
-	asl a
-	sta $96ad
-	lda $96ae
-	rol a
-	bcc L9639
+	adc cs_16_h		; handle overflow
+	sta cs_16_h
 	
-	inc $96ab
+	pla			; restore the original byte
+
+	; calculate secondary checksum
+	clc
+	adc L96ad		; add new byte to running checksum
+	asl a			;  and multiply by two
+	sta L96ad
+	lda L96ae		; get high byte
+	rol a			;  rotate carry in
+	bcc L9639		;  skip if no overflow
+	
+	inc L96ab		; increment if overflowed
 	bne L9639
 	
-	inc $96ac
+	inc L96ac		;  handle overflow
 L9639
-	sta $96ae
-	iny
-	bne L9610
+	sta L96ae
+	iny			; increment index
+	bne L9610		;  if we didn't overflow, loop
+	inc $32			; fix up high byte of vector
+	dex			; decrement page count
+	bne L9610		;  and run until we're out of pages
 	
-	inc $32
-	dex
-	bne L9610
+	ldx lba_hi		; get our block back
+	ldy lba_lo
+	inx			;  select next block
+	bne L964e		;  and head back if there's no overflow
 	
-	ldx $96a8
-	ldy L96a7
-	inx
-	bne L964e
-	
-	iny
+	iny			;  handle block low byte overflow by incrementing high byte
 L964e
 	tya
 	bne L9660
 	
-	cpx #$1a
+	cpx #$1a		; luchange.r
 	bne L9656
 	
 	inx
 L9656
-	cpx #$ee
+	cpx #$ee		; DISCBITMAP
 	bne L966c
 	
 	ldx #$ef
@@ -87,51 +118,60 @@ L9660
 	cpy #$02
 	bne L966c
 	
-	cpx #$9e
+	cpx #$9e		; defaults.r
 	bne L966c
 	
 	ldx #$2e
 	ldy #$03
 L966c
-	cpy $9274
+	cpy LTK_FileHeaderBlock+$94 ; 9274
 	beq L9674
 L9671
 	jmp L95f5
                     
 L9674
-	cpx $9275
+	cpx LTK_FileHeaderBlock+$95 ; 9275
 	bne L9671
 	ldy #$05
 L967b
-	lda $9276,y
-	cmp $96a9,y
-	bne L969c
+	lda LTK_FileHeaderBlock+$96,y ; 9276
+	cmp cs_16_l,y
+	bne ChksumBad
 	dey
 	bpl L967b
 	
-	dec $920a
-	lda #$0a
+	dec LTK_FileHeaderBlock+$2A ; 920a
+	lda #$0a		; LU 10
 	ldx #$00
-	ldy #$00
-	sec
-	jsr LTK_HDDiscDriver
-	.byte <LTK_FileHeaderBlock,>LTK_FileHeaderBlock,$01
+	ldy #$00		; block 0
+	sec			; write
+	jsr LTK_HDDiscDriver	;  WRITE sector 0, LU 10
+	.word LTK_FileHeaderBlock
+	.byte $01
 	
-L9696
+ChksumGood			; Checksum is okay.  Print the okay message now
 	ldx #<str_ChecksumsGood
 	ldy #>str_ChecksumsGood
 	bne L96a3
 	
-L969c
-	jsr Beep_If_Allowed
+ChksumBad			; Checksums failed; warn the user and beep
+	jsr Beep_If_Allowed	;  but not necessarily in that order.
 	ldx #<str_ChecksumsBad
 	ldy #>str_ChecksumsBad
 L96a3
 	jsr LTK_Print
 	rts
                     
-L96a7
-	.byte $00,$00,$00,$00,$00,$00,$00,$00 
+lba_lo
+	.byte $00
+lba_hi	.byte $00
+cs_16_l	.byte $00		; 16bit checksum
+cs_16_h	.byte $00
+L96ab	.byte $00
+L96ac	.byte $00
+L96ad	.byte $00
+L96ae	.byte $00 
+
 str_Checksumming ;$96af
 	.text "{clr}{return}{return}please wait... checksumming the dos."
 	.byte $00
